177257a91a5281c8e187c568fa8aa7510c94f433
davglick <dav.glick18@gmail.com>
2016-10-28 18:32:57 +1100

    Initial Commit

---
+++ Appear_Ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Appear_Ios
+//
+//  Created by Davin Glick on 28/10/16.
+//  Copyright © 2016 Appear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
